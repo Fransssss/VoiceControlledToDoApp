227b9740dfb668ed4c9b65eaa82c12266c9d117a
Fransiskus Agapa <agapaf@spu.edu>
2025-05-10 13:14:51 -0700

    list task to finish in script

--- docs/script.js
+++ docs/script.js
@@ -1,1 +1,31 @@
 const SpeechRecognition = window.SpeechRecognition || window.webkitSpeechRecognition;
+
+if(!SpeechRecognition){
+    // TODO: use custom pop-up instead for friendly outlook
+    alert("Oops, sorry your browser does not seem to support speech recognition")
+    console.log("! Oops, sorry your browser does not seem to support speech recognition");
+} else {
+    console.log("> Hey, speech recognition initiated");
+
+    // 1. Add task 
+    // When task exist, allow duplication ?
+        // Yes, add duplicate
+        // No,  do not do anything
+    
+    // 2. Complete task
+        // Complete by (full) task name
+        // Complete task by number
+            // Remove associated task if exist
+            // Popup shows up - 'task does not exist'
+    
+    // 3. Remove task 
+        // Remove by (full) task name 
+        // Remove by associated number 
+            // Remove associated task if exist
+            // Popup shows up - 'task does not exist'
+            
+        
+    
+    
+
+}
